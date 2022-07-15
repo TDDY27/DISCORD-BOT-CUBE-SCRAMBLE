22506dacf0a52983b1a9d0cd613703cacc8c3dd3
TDDY <96487789+TDDY27@users.noreply.github.com>
2022-07-15 13:06:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print ("hello world")
+import discord
+from discord.ext import commands
+
+bot=commands.Bot(command_prefix='-')
+
+@bot.event
+async def on_ready():
+    print(">>Bot is online<<")
+
+bot.run("OTk2NzgyNjkxMjE3MzA5NzI2.GkVLih.8Noq9FH7Iq-g90oEoIDjJ3Ps8b8oJ7Ijd_tPxs")
+
+
